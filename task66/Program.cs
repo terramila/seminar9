9dd415fb4480f43bf4c0e02000f061b0be075872
tarana <beshirova@hotmail.com>
2022-10-19 09:07:43 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,33 @@
+﻿/*: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+ элементов в промежутке от M до N. Рекурсивно
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+
+}
+
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+Console.Write($"{m}, {n} ->");
+PrintSum(m, n, temp = 0);
